d3cb83b4ea166564b607e1e75c98aad60a202757
John Flatness <john@zerocrates.org>
2018-10-22 17:59:17 -0400

    Update npm deps

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('css', function () {
             includePaths: ['node_modules/susy/sass']
         }).on('error', sass.logError))
         .pipe(postcss([
-            autoprefixer({browsers: ['> 5%', '> 5% in US', 'last 2 versions']})
+            autoprefixer()
         ]))
         .pipe(gulp.dest('./asset/css'));
 });
